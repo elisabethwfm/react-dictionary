4f2d9cb79da9285e2dd35143fb3ee21e88dab4e3
elisabethwfm <elisabeth.wolfram@gmail.com>
2022-05-18 15:45:26 +0200

    added the search field and styling

--- src/DictionarySearch.js
+++ src/DictionarySearch.js
@@ -3,7 +3,15 @@ import React from "react";
 export default function DictionarySearch() {
   return (
     <div className="dictionarySearch">
-      <h1>Search</h1>
+      <h1>Dictionary App</h1>
+      <form>
+        <input
+          type="text"
+          className="dictionary"
+          placeholder="Type a word..."
+        ></input>
+        <span className="search-btn">🔍</span>
+      </form>
     </div>
   );
 }
