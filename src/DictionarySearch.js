7bbb2a7693b3f561bc16a857de991aa20aa89585
elisabethwfm <elisabeth.wolfram@gmail.com>
2022-05-18 16:10:36 +0200

    changed the styling and prepraed a function for the search term

--- src/DictionarySearch.js
+++ src/DictionarySearch.js
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function DictionarySearch() {
+  let [searchTerm, setSearchTerm] = useState("");
+
+  function search(event) {
+    event.preventDefault();
+    alert(`Searching for ${searchTerm}`);
+  }
+
+  function handleSearchTerm(event) {
+    console.log(event);
+  }
+
   return (
     <div className="dictionarySearch">
       <h1>Dictionary App</h1>
-      <form>
+      <form onSubmit={search}>
         <input
           type="search"
           className="dictionary"
           placeholder="Type a word..."
+          onChange={handleSearchTerm}
         ></input>
         <span className="search-btn">🔍</span>
       </form>
